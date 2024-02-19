183d65dd84ddd9ad2bb52748398f2035d21ea710
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-19 09:47:44 +0100

    Route and Controller for Posts created

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,15 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MicroPostController extends AbstractController
 {
-    #[Route('/micro/post', name: 'app_micro_post')]
-    public function index(): Response
+    #[Route('/micro-posts', name: 'app_micro_post')]
+    public function index(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
+        // $posts = $postRepository->findByCreatedAt(['createdAt' => 'DESC']);
+        // dd($posts);
+        
+        return $this->render('micro_post/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    #[Route('/micro-posts/{id}', name: 'app_micro_post_show')]
+    public function show(PostRepository $postRepository, $id): Response
+    {
+        $post = $postRepository->findById($id);
+        dd($post);
+        
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,42 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Post;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        for ($p = 0; $p < mt_rand(10, 20); $p++) {
+            $post = new Post();
+            $words = $faker->words;
+            $post->setTitle(implode(' ', $words))
+                 ->setContent($faker->realText())
+                 ->setCreatedAt($faker->dateTimeBetween('-100 days', '-1 days'));
+            $manager->persist($post);
+        }
+        // $post1 = new Post();
+        // $post1->setTitle('My title 1');
+        // $post1->setContent('This is my first title');
+        // $post1->setCreatedAt(new DateTime());
+        // $manager->persist($post1);
+
+        // $post2 = new Post();
+        // $post2->setTitle('My title 2');
+        // $post2->setContent('This is my first title');
+        // $post2->setCreatedAt(new DateTime());
+        // $manager->persist($post2);
+
+        // $post3 = new Post();
+        // $post3->setTitle('My title 3');
+        // $post3->setContent('This is my first title');
+        // $post3->setCreatedAt(new DateTime());
+        // $manager->persist($post3);
 
         $manager->flush();
     }
